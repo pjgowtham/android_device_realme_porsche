514530a4570ff9af363555a8c955ae5a6a1bba99
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 01:45:16 +0300

    porsche: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,8 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_porsche.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_porsche-user \
-    lineage_porsche-userdebug \
-    lineage_porsche-eng
